fce2258aec460eca742592c391a8b0a3b2260505
charliejl28 <cmj3@princeton.edu>
2014-08-02 19:55:42 -0400

    parse JSON

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,69 @@
-// port status change
-function portStatusChange(JSON){
-	
-	// parse JSON
+var ports = {};
+var packets = {};
 
-	// notify web
-	setStatus(portIDfrom, 1, packet)	// broadcasting port
-	setStatus(portIDto, 2, packet)		// receiving port
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                parseJSON(allText);
+            }
+        }
+    }
+    rawFile.send(null);
 }
+
+// parse JSON
+function parseJSON(JSON_string){
+
+	console.log("parsing JSON: " + JSON_string);
+
+	// json object
+	var JSON_object = JSON.parse(JSON_string);
+	console.log("parsed json ");
+
+	// all packets
+	var packetsJSON = JSON_object.packets;
+	for	(index = 0; index < packetsJSON.length; index++) {
+		packetJSON = packetsJSON[index];
+		var id = packetJSON["id"];
+		var fileName = packetJSON["file"];
+		packets[id] = fileName;
+	}
+
+	console.log("parsed packets");
+
+	// all ports
+	var portsJSON = JSON_object.ports;
+	for (index = 0; index < portsJSON.length; index++){
+		portJSON = portsJSON[index];
+		var portID = portJSON["id"];
+		var portStatus = portJSON["status"];
+		var packetID = portJSON["packet"];
+
+		// update node for existing port
+		if (portID in ports){
+			setStatus(portID, portStatus, packets[packetID]);
+		}
+
+		// create new node
+		else {
+			addNode(portID, portStatus, packets[packetID]);
+			ports[portID] = portStatus;
+		}
+
+	}
+}
+
+console.log("running dashboard.js");
+
+
+readTextFile("network.json");
+
 
