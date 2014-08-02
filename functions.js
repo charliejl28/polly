b37f2e1d927de2b1a2ea9286d83c71f5c2ecad14
AlexEKoren <alex.koren@jhu.edu>
2014-08-02 16:38:47 -0700

    some changes

--- functions.js
+++ functions.js
@@ -19,14 +19,17 @@ function addNode(id, status, packet) {
   var node_status_id = "node-status-" + id;
   var node_title_id = "node-title-" + id;
   var node_file_id = "node-title-" + id;
-  var div = '<div class = "node">\n<div class = "node-status waiting" id = "';
+  var div = '<div class = "node">\n<div class = "node-status ' + status + '" id = "';
   div += node_status_id;
   div += '">\n<div class = "node-file" id = "';
   div += node_file_id;
-  div += '">\n No File \n</div>\n</div>\n<div class = "node-title" id = "';
+  div += '">\n' + packet + '\n</div>\n</div>\n<div class = "node-title" id = "';
   div += node_title_id;
   div += '">\n Node ' + id + '\n</div>\n</div>';
   $('.node-container').append(div);
   
   $('.node-container').width($('.node-container').width() + 300);
 }
+
+addNode(2, 'downloading', 'hello');
+addNode(1, 'waiting', 'No file');
