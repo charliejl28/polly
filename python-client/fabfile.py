355248f66c39c1876eaee4287ebdfdb03c8518fb
Shantanu Bala <shantanubala@gmail.com>
2014-08-02 23:49:31 -0700

    added deployment script

---
+++ python-client/fabfile.py
@@ -0,0 +1,22 @@
+from fabric.api import *
+from fabric.contrib.files import exists
+from fabric.context_managers import settings
+import os
+
+import json
+
+nf = open('nodes.json')
+nodes = json.loads(nf.read())
+
+env.hosts = [n for n in nodes]
+env.user = "root"
+
+def deploy():
+	with cd("/srv/polly"):
+		sudo("git checkout .")
+		sudo("git pull origin master")
+
+	sudo("service uwsgi restart")
+
+def update_auth_keys():
+	sudo("cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys")
