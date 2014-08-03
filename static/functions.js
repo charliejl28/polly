7b331285576f4f3615c8e253316e14756d23ee96
AlexEKoren <alex.koren@jhu.edu>
2014-08-03 09:50:39 -0700

    loading changes

--- static/functions.js
+++ static/functions.js
@@ -49,50 +49,25 @@ function connect(id1, id2) {
 	}
 }
 
-var colors = new Array(
-  [0,0,0],
-  [45,175,230]);
-
 var step = 0;
-var colorIndices = [0,1,0,1];
 var gradientSpeed = 0.05;
 function updateGradient() {
-	var c0_0 = colors[colorIndices[0]];
-	var c0_1 = colors[colorIndices[1]];
-	var c1_0 = colors[colorIndices[2]];
-	var c1_1 = colors[colorIndices[3]];
-
-	var istep = 1 - step;
-	var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-	var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-	var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-	var color1 = "#"+((r1 << 16) | (g1 << 8) | b1).toString(16);
-
-	var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-	var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-	var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-	var color2 = "#"+((r2 << 16) | (g2 << 8) | b2).toString(16);
-
+	var r = 255 * Math.abs(1 - step);
+	var g = 255 * Math.abs(1 - step);
+	var b = 255 * Math.abs(1 - step);
+	var color1 = "#"+((r << 16) | (g << 8) | b).toString(16);
+	r = 200 * Math.abs(1 - step);
+	g = 200 * Math.abs(1 - step);
+	b = 255 * Math.abs(1 - step);
+	var color2 = "#"+((r << 16) | (g << 8) | b).toString(16);
 	$('.dot').css({
 		background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
 		background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"
 	});
-	/*$('.leftline').css({
-		background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-		background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"
-	});*/
-  
 	step += gradientSpeed;
-	if ( step >= 1 )
-	{
-		step %= 1;
-		colorIndices[0] = colorIndices[1];
-		colorIndices[2] = colorIndices[3];
-
-		colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-		colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-
+	if (step >= 2) {
+		step = 0;
 	}
 }
 
-setInterval(updateGradient,100);
+setInterval(updateGradient,50);
